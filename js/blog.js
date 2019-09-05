8db87d6191cb07b56d6928bc443d7bfe727754ec
laugoabad <36644178+laugoabad@users.noreply.github.com>
2019-09-05 16:41:48 -0300

    secondcommit

--- js/blog.js
+++ js/blog.js
@@ -6,23 +6,27 @@ const app = new Vue({
             {
                 autor: "Laura González", 
                 titulo: "TITULO", 
-                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida."
+                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida.",
+                hrefer: "./cards/card-1.html"
             },
 
             {
                 autor: "Laura González",
                 titulo: "TITULO",
-                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida."
+                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida.",
+                hrefer: './cards/card-2.html'
             },
             {
                 autor: "Laura González",
                 titulo: "TITULO",
-                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida."
+                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida.",
+                hrefer: './cards/card-3.html'
             },
             {
                 autor: "Laura González",
                 titulo: "TITULO",
-                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida."
+                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida.",
+                hrefer: './cards/card-4.html'
             },
         ]
     }
