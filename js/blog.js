2c45801d25ea3afb700e4e9665d7118be5595828
laugoabad <36644178+laugoabad@users.noreply.github.com>
2019-09-05 15:43:38 -0300

    First commit

---
+++ js/blog.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        tituloBlog: "Blog",
+        notas: [
+            {
+                autor: "Laura González", 
+                titulo: "TITULO", 
+                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida."
+            },
+
+            {
+                autor: "Laura González",
+                titulo: "TITULO",
+                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida."
+            },
+            {
+                autor: "Laura González",
+                titulo: "TITULO",
+                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida."
+            },
+            {
+                autor: "Laura González",
+                titulo: "TITULO",
+                texto: "Lorem ipsum dolor sit amet consectetur adipiscing elit vel ridiculus justo, vulputate consequateget pulvinar euismod natoque praesent dapibus cubilia placerat integer, tempus magna ad accumsan eros risus etiamvvestibulum  Curabitur maecenas habitant nascetur venenatis class convallis feugiat odio euismod, nec ullamcorper faucibus sollicitudin nisl a suscipit ui, platea torquent congue per libero felis donec aptent.Conubia litora ligula velit facilisis maecenas mauris sed in eros consequat viverra justo eu orci, leo antevestibulum netus varius mus hendrerit purus pretium nullam sociis pellentesque gravida."
+            },
+        ]
+    }
+})
